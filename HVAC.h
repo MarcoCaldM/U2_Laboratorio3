60b6c0b1350769c788978ace0e70b6868d235492
Marco Calderón <mcacae.2@gmail.com>
2023-10-28 17:56:47 -0600

    first commit: Botón ON_OFF

---
+++ HVAC.h
@@ -0,0 +1,144 @@
+ // FileName:        HVAC.h
+ // Dependencies:    None.
+ // Processor:       MSP432
+ // Board:           MSP432P401R
+ // Program version: CCS V8.3 TI
+ // Company:         Texas Instruments
+ // Description:     Incluye librer�as, define ciertas macros y significados as� como llevar un control de versiones.
+ // Authors:         Jos� Luis Chac�n M. y Jes�s Alejandro Navarro Acosta.
+ // Updated:         11/2018
+
+#ifndef _hvac_h_
+#define _hvac_h_
+
+#pragma once
+
+#define __MSP432P401R__
+#define  __SYSTEM_CLOCK    48000000 // Frecuencias funcionales recomendadas: 12, 24 y 48 Mhz.
+
+/* Archivos de cabecera importantes. */
+#include <unistd.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Archivos de cabecera POSIX. */
+#include <pthread.h>
+#include <semaphore.h>
+#include <ti/posix/tirtos/_pthread.h>
+#include <ti/sysbios/knl/Task.h>
+
+/* Archivos de cabecera para RTOS. */
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Event.h>
+
+/* Board Support Package. */
+#include "Drivers/BSP.h"
+
+/* Enumeradores para la descripci�n del sistema. */
+
+enum FAN        // Para el fan (abanico).
+{
+    On,
+    Auto,
+};
+
+enum SYSTEM     // Para el sistema cuando FAN est� en auto (cool, off y heat, o no considerar ninguno y usar fan only).
+{
+    Cool,
+    Off,
+    Heat,
+    FanOnly,
+};
+
+struct EstadoEntradas
+{
+    uint8_t  SystemState;
+    uint8_t     FanState;
+}   EstadoEntradas;
+
+// Definiciones B�sicas.
+#define ENTRADA 1
+#define SALIDA 0
+
+// Re-definici�n de los bits y puertos de entrada a utilizar.
+#define FAN_ON      B3
+#define FAN_AUTO    B4
+#define SYSTEM_COOL B5
+#define SYSTEM_OFF  B6
+#define SYSTEM_HEAT B7
+#define ON_OFF      B1
+#define SP_DOWN     B4
+
+#define FAN_PORT        2
+#define SYSTEM_PORT     2
+#define SETPOINT_PORT   1
+#define FAN_PORTT        P2
+#define SYSTEM_PORTT     P2
+#define SETPOINT_PORTT   P1
+
+#define TEMP_CH         CH0
+#define HEARTBEAT_CH    CH1
+#define POT_PIN         AN1
+
+// Re-definici�n de los bits y puertos de salida a utilizar.
+#define FAN_LED     BSP_LED1
+#define HEAT_LED    BSP_LED2
+#define HBeatLED    BSP_LED3
+#define COOL_LED    BSP_LED4
+
+#define FAN_LED_PORT    BSP_LED1_PORT
+#define HEAT_LED_PORT   BSP_LED2_PORT
+#define HB_LED_PORT     BSP_LED3_PORT
+#define COOL_LED_PORT   BSP_LED4_PORT
+
+// Definiciones del estado 'normal' de los botones externos a la tarjeta (solo hay dos botones).
+#define GND 0
+#define VCC 1
+#define NORMAL_STATE_EXTRA_BUTTONS GND  // Aqui se coloca GND o VCC.
+
+// Definiciones del sistema.
+#define MAX_MSG_SIZE 64
+#define MAX_ADC_VALUE 16383             // (2 ^14 bits) es la resoluci�n default.
+#define MAIN_UART (uint32_t)(EUSCI_A0)
+#define DELAY 20000
+#define ITERATIONS_TO_PRINT 49
+
+// Definiciones para el encendido y apagado del sistema
+uint8_t Enc_Apg;
+uint32_t contadorApg;
+#define ENCENDIDO 1
+#define APAGADO 0
+
+// Definici�n para el RTOS.
+#define THREADSTACKSIZE1 1500
+
+/* Funciones. */
+
+/* Funci�n de interrupci�n para botones de setpoint. */
+extern void INT_SWI(void);
+
+/* Funciones de inicializaci�n. */
+extern void HVAC_InicialiceIO   (void);
+extern void HVAC_InicialiceADC  (void);
+extern void HVAC_InicialiceUART (void);
+extern void System_InicialiceTIMER (void); // ESTO LO A�ADI PARA UTILIZAR EL "TIMER32"
+
+/* Funciones principales. */
+extern void HVAC_ActualizarEntradas(void);
+extern void HVAC_ActualizarSalidas(void);
+extern void HVAC_Heartbeat(void);
+extern void HVAC_PrintState(void);
+
+// Funciones para los estados Heat y Cool.
+extern void HVAC_Heat(void);
+extern void HVAC_Cool(void);
+
+// Funciones para incrementar o disminuir setpoint.
+extern void HVAC_Enc_Apg(void);
+extern void HVAC_SetPointDown(void);
+
+#endif
---
+++ HVAC_Thread.c
@@ -0,0 +1,70 @@
+ // FileName:        HVAC_Thread.c
+ // Dependencies:    HVAC.h
+ // Processor:       MSP432
+ // Board:           MSP432P401R
+ // Program version: CCS V8.3 TI
+ // Company:         Texas Instruments
+ // Description:     Definici�n de las funciones del thread de HVAC_Thread().
+ // Authors:         Jos� Luis Chac�n M. y Jes�s Alejandro Navarro Acosta.
+ // Updated:         11/2018
+
+#include "HVAC.h"                           // Incluye definici�n del sistema.
+
+/*****  SE DECLARARON LAS VARIABLES Y FUNCIONES PARA REALIZAR EL DALAY CON EL TIMER ******** */
+bool retraso = false;
+void Timer32_INT1 (void); // Funci�n de interrupci�n.
+void Delay_ms (uint32_t time); // Funci�n de delay.
+/***********************************************************************************************/
+
+/*********************************THREAD******************************************
+ * Function: HVAC_Thread
+ * Preconditions: None.
+ * Overview: Realiza la lectura de la temperatura y controla salidas actualizando
+ *           a su vez entradas. Imprime estados. Tambi�n contiene el heartbeat.
+ * Input:  Apuntador vac�o que puede apuntar cualquier tipo de dato.
+ * Output: None.
+ *
+ ********************************************************************************/
+
+void *HVAC_Thread(void *arg0)
+{
+    SystemInit();
+    System_InicialiceTIMER();           // INICIALIZA EL TIMER
+    HVAC_InicialiceIO();                // INICIALIZA ENTRADAS Y SALIDAS
+    HVAC_InicialiceADC();               // INICIALIZA MODULO ADC
+    HVAC_InicialiceUART();              // INICIALIZA MODULO UART
+
+    GPIO_setOutput(BSP_LED1_PORT,  BSP_LED1,  0); //Apaga el LED rojo
+    Enc_Apg = APAGADO;
+
+    while(1)
+    {
+        contadorApg = 0;        //Reinicia el contador del apagado
+        while(Enc_Apg == ENCENDIDO){
+            HVAC_ActualizarEntradas();
+            HVAC_ActualizarSalidas();
+            HVAC_PrintState();
+            HVAC_Heartbeat();
+        }
+    }
+}
+
+
+
+/* *********  FUNCIONES PARA REALIZAR EL DALAY CON EL TIMER ********* */
+void Delay_ms(uint32_t time)
+{
+    T32_EnableTimer1(); // Habilita timer.
+    T32_EnableInterrupt1(); // Habilita interrupci�n.
+    // Carga de valor en milisegundos.
+    T32_SetLoadValue1(time*(__SYSTEM_CLOCK/1000));
+    retraso = true;
+    while(retraso); // While enclavado.
+}
+void Timer32_INT1(void)
+{
+    T32_ClearInterruptFlag1(); // Al llegar a la interrupci�n
+    retraso = false; // desenclava el while.
+    contadorApg = 0; // Si no se pulsa dos veces el boton ON/OFF reinicia el contador de apagado
+}
+
